c37950b3856d01d524240ae1fdbb7ed7dea684d8
unshaven <31883567+unshaven@users.noreply.github.com>
2018-02-02 20:04:56 +0800

    Create 169_Majority Element.py

---
+++ 169_Majority Element.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#author :wuji
+#data:2018-2-2
+#difficulty degree：
+#problem: 168_Excel Sheet Column Title
+#time_complecity:  
+#space_complecity: 
+#beats: 
+'''
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+For example:
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB 
+'''
