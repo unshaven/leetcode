be6712188c21e6c6f352b234b1134bca49f5f642
unshaven <31883567+unshaven@users.noreply.github.com>
2018-03-29 00:01:20 +0800

    Update 290. Word Pattern.py

--- 290. Word Pattern.py
+++ 290. Word Pattern.py
@@ -1,1 +1,19 @@
+'''
+Given a pattern and a string str, find if str follows the same pattern.
 
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str.
+
+Examples:
+pattern = "abba", str = "dog cat cat dog" should return true.
+pattern = "abba", str = "dog cat cat fish" should return false.
+pattern = "aaaa", str = "dog cat cat dog" should return false.
+pattern = "abba", str = "dog dog dog dog" should return false.
+Notes:
+You may assume pattern contains only lowercase letters, and str contains lowercase letters separated by a single space.
+'''
+一种用map的函数应该记住
+def wordPattern(self, pattern, str):
+  l = str.split()
+  s = pattern
+  return map(s.find,s) == map(l.index,l)
+def 
